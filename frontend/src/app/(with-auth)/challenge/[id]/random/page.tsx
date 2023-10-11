cccc3c21f9442cb96f637d193b94fde5c2e4048c
Filipe Ferreira <filipeferreira96@hotmail.com>
2023-10-11 21:23:04 +0100

    logic updated

--- frontend/src/app/(with-auth)/challenge/[id]/random/page.tsx
+++ frontend/src/app/(with-auth)/challenge/[id]/random/page.tsx
@@ -1,11 +1,13 @@
 "use client";
-import React, { useEffect, useState, } from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Title, TextInput, Loader } from "@mantine/core";
 import classes from "./random.module.scss";
+import { jsQuizz } from "./data";
 
 interface Question {
+  id: number;
   question: string;
-  choices: string[];
+  choices?: string[];
   correctAnswer: string;
   type: string;
 }
@@ -14,6 +16,7 @@ interface Result {
   score: number;
   correctAnswers: number;
   wrongAnswers: number;
+  userAnswers: { id: number; answer: string }[];
 }
 
 interface RandomProps {
@@ -24,28 +27,26 @@ interface RandomProps {
 
 const Random: React.FC<RandomProps> = ({ params: { id } }: RandomProps) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [answerIdx, setAnswerIdx] = useState<number | null>(null);
-  const [answer, setAnswer] = useState<boolean | undefined>(undefined);
   const [result, setResult] = useState<Result>({
     score: 0,
     correctAnswers: 0,
     wrongAnswers: 0,
+    userAnswers: [],
   });
   const [showResult, setShowResult] = useState(false);
   const [showAnswerTimer, setShowAnswerTimer] = useState(true);
-  const [inputAnswer, setInputAnswer] = useState("");
   const [questions, setQuestions] = useState<Question[]>([]);
   const [loading, setLoading] = useState(true);
 
-  console.log(result)
-  console.log(inputAnswer)
+  console.log(result);
+
   useEffect(() => {
     const getQuestions = async () => {
       try {
-        const response = await fetch(`https://644982a3e7eb3378ca4ba471.mockapi.io/questions`);
-        const questionsResponse = await response.json();
+        /* const response = await fetch(`https://644982a3e7eb3378ca4ba471.mockapi.io/questions`);
+        const questionsResponse = await response.json(); */
 
-        setQuestions(questionsResponse);
+        setQuestions(jsQuizz);
         setLoading(false);
       } catch (error) {
         console.log(error);
@@ -59,40 +60,66 @@ const Random: React.FC<RandomProps> = ({ params: { id } }: RandomProps) => {
     return <Loader />;
   }
 
-  const { question, choices, correctAnswer, type } = questions[currentQuestion];
-  console.log(question, choices, correctAnswer, type);
-  const onAnswerClick = (answer: string, index: number) => {
-    setAnswerIdx(index);
-    if (answer === correctAnswer) {
-      setAnswer(true);
-    } else {
-      setAnswer(false);
-    }
+  const { id: questionId, question, choices, correctAnswer, type } = questions[currentQuestion];
+
+  const handleChoiceSelection = (chosenAnswer: string) => {
+
+    setResult((prevResult) => {
+      const updatedUserAnswers = prevResult.userAnswers.slice(); // Create a copy of the array
+      const existingAnswer = updatedUserAnswers.find((answer) => answer.id === questionId);
+
+      if (existingAnswer) {
+        existingAnswer.answer = chosenAnswer;
+      } else {
+        updatedUserAnswers.push({ id: questionId, answer: chosenAnswer });
+      }
+
+      return {
+        ...prevResult,
+        userAnswers: updatedUserAnswers,
+      };
+    });
   };
 
-  const onClickNext = (finalAnswer: boolean | undefined) => {
-    setAnswerIdx(null);
-    setShowAnswerTimer(false);
-    setInputAnswer("");
-    setResult((prev) =>
-      finalAnswer
-        ? {
-            ...prev,
-            score: prev.score + 5,
-            correctAnswers: prev.correctAnswers + 1,
-          }
-        : {
-            ...prev,
-            wrongAnswers: prev.wrongAnswers + 1,
-          }
-    );
+  const onClickNext = (id: number) => {
+    setCurrentQuestion(id);
+  };
 
-    if (currentQuestion !== questions.length - 1) {
-      setCurrentQuestion((prev) => prev + 1);
-    } else {
-      setCurrentQuestion(0);
-      setShowResult(true);
-    }
+  const endChallenge = () => {
+    let score = 0;
+    let correctAnswers = 0;
+    let wrongAnswers = 0;
+
+    result.userAnswers.forEach((userAnswer) => {
+      const question = questions.find((q) => q.id === userAnswer.id);
+
+      if (question) {
+        if (question.type === "FIB") {
+          if (userAnswer.answer.trim().toLowerCase() === question.correctAnswer.trim().toLowerCase()) {
+            score += 5;
+            correctAnswers += 1;
+          } else {
+            wrongAnswers += 1;
+          }
+        } else {
+          if (userAnswer.answer === question.correctAnswer) {
+            score += 5;
+            correctAnswers += 1;
+          } else {
+            wrongAnswers += 1;
+          }
+        }
+      }
+    });
+
+    setResult({
+      score,
+      correctAnswers,
+      wrongAnswers,
+      userAnswers: result.userAnswers,
+    });
+
+    setShowResult(true);
   };
 
   setTimeout(() => {
@@ -104,33 +131,44 @@ const Random: React.FC<RandomProps> = ({ params: { id } }: RandomProps) => {
       score: 0,
       correctAnswers: 0,
       wrongAnswers: 0,
+      userAnswers: [],
     });
     setShowResult(false);
   };
 
-  const handleTimeUp = () => {
-    setAnswer(false);
-    onClickNext(false);
-  };
-
   const handleInputChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    setInputAnswer(evt.target.value);
+    const userInput = evt.target.value;
 
-    if (evt.target.value === correctAnswer) {
-      setAnswer(true);
-    } else {
-      setAnswer(false);
-    }
+    setResult((prevResult) => {
+      const updatedUserAnswers = prevResult.userAnswers.slice(); // Create a copy of the array
+
+      // Check if the current question id is already in userAnswers
+      const existingAnswer = updatedUserAnswers.find((answer) => answer.id === questionId);
+
+      if (existingAnswer) {
+        // If it exists, update the answer
+        existingAnswer.answer = userInput;
+      } else {
+        // If it doesn't exist, add a new entry
+        updatedUserAnswers.push({ id: questionId, answer: userInput });
+      }
+
+      return {
+        ...prevResult,
+        userAnswers: updatedUserAnswers,
+      };
+    });
   };
 
   const getAnswerUI = () => {
     if (type === "FIB") {
-      return <input value={inputAnswer} onChange={handleInputChange} />;
+      return <input value={result.userAnswers[currentQuestion]?.answer || ""} onChange={handleInputChange} />;
     }
+
     return (
       <ul>
-        {choices.map((answer, index) => (
-          <li onClick={() => onAnswerClick(answer, index)} key={answer} className={answerIdx === index ? classes.selectedAnswer : undefined}>
+        {choices?.map((answer, index) => (
+          <li onClick={() => handleChoiceSelection(answer)} key={index} className={result.userAnswers[currentQuestion]?.answer === answer ? classes.selectedAnswer : undefined}>
             {answer}
           </li>
         ))}
@@ -143,20 +181,27 @@ const Random: React.FC<RandomProps> = ({ params: { id } }: RandomProps) => {
       {!showResult ? (
         <>
           {/* {showAnswerTimer && <AnswerTimer duration={10} onTimeUp={handleTimeUp} />} */}
-          <p className={classes.answer}>{answer}</p>
+          
           <span className={classes.activeQuestionNo}>{currentQuestion + 1}</span>
           <span className={classes.totalQuestion}>/{questions.length}</span>
           <h2 className={classes.question}>{question}</h2>
           {getAnswerUI()}
 
           <div className={classes.footer}>
-            <button
-              onClick={() => onClickNext(answer)}
-              disabled={answerIdx === null && !inputAnswer} // disables the Next button until there is an answer.
-              className={classes.button}
-            >
-              {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
-            </button>
+            {currentQuestion > 0 && (
+              <button onClick={() => onClickNext(currentQuestion - 1)} className={classes.button}>
+                Back
+              </button>
+            )}
+            {currentQuestion === questions.length - 1 ? (
+              <button onClick={() => endChallenge()} disabled={false} className={classes.button}>
+                Finish
+              </button>
+            ) : (
+              <button onClick={() => onClickNext(currentQuestion + 1)} disabled={false} className={classes.button}>
+                Next
+              </button>
+            )}
           </div>
         </>
       ) : (
