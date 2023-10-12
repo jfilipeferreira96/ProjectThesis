feecca5a5acde3f0abcd58fbef849557fa7bc434
José Filipe Ferreira <jose.ferreira@i-charging.pt>
2023-10-12 09:13:26 +0100

    cors urls

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,10 @@ app.use("/views", express.static("views"));
 app.use(morgan("dev"));
 
 const corsOptions = {
-  origin: ["http://localhost:3000", "http://localhost:3001"],
+  origin: [
+    "http://localhost:3000", "http://localhost:3001",
+    "http://127.0.0.1:3000", "http://127.0.0.1:3001"
+  ],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
