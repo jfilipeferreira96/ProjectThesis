461284ed888d8fc4f0cd75a9a5396d39a91390a1
Filipe Ferreira <filipeferreira96@hotmail.com>
2023-10-28 15:49:32 +0100

    landing page new stuff

--- frontend/src/app/(no-auth)/page.tsx
+++ frontend/src/app/(no-auth)/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 import AffixComponent from "@/components/affix";
 import { FaqSimple } from "@/components/landing-page/sections/faq";
+import { FeaturesCards } from "@/components/landing-page/sections/feature-cards";
 import Footer from "@/components/landing-page/sections/footer";
 import { HeaderMenu } from "@/components/landing-page/sections/header";
+import { Hero } from "@/components/landing-page/sections/hero";
 import { Affix, Button, Group, useMantineColorScheme } from "@mantine/core";
 
 export default function Home() {
@@ -11,6 +13,8 @@ export default function Home() {
   return (
     <>
       <AffixComponent />
+      <Hero />
+      <FeaturesCards />
       <FaqSimple />
       <Footer />
     </>
