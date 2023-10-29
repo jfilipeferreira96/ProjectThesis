28c12cc4c18081ec3987a3f63b0567540690527d
Filipe Ferreira <filipeferreira96@hotmail.com>
2023-10-29 18:23:49 +0000

    new model quizresponse

---
+++ backend/src/models/quizzResponse.model.ts
@@ -0,0 +1,35 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+interface IQuizResponse extends Document {
+  quiz: Schema.Types.ObjectId;
+  user: Schema.Types.ObjectId;
+  answers: string[];
+  score: number; 
+  lastUpdate: Date; 
+}
+
+const QuizResponseSchema: Schema<IQuizResponse> = new mongoose.Schema({
+  quiz: {
+    type: Schema.Types.ObjectId,
+    ref: "Quiz",
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  answers: [String],
+  score: {
+    type: Number,
+    required: true,
+  },
+  lastUpdate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const QuizResponse: Model<IQuizResponse> = mongoose.model<IQuizResponse>("QuizResponse", QuizResponseSchema);
+
+export default QuizResponse;
