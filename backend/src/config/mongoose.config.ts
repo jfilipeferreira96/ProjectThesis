e7c6fa08621803de1da7028f913119c81a631bcb
Filipe Ferreira <filipeferreira96@hotmail.com>
2024-04-06 15:49:43 +0100

    docker-finnaly-configured

--- backend/src/config/mongoose.config.ts
+++ backend/src/config/mongoose.config.ts
@@ -4,22 +4,15 @@ import Logger from "../utils/logger";
 
 const connectToDatabase = async (retryCount = 0) => {
   const options: ConnectOptions = {};
-  const uris = [config.local.localUrlDatabase ?? "mongodb://localhost:27017/docker", "mongodb://mongo:27017/docker", "mongodb://127.0.0.1:27017/docker"];
+  const uri = config.local.localUrlDatabase ?? "mongodb://mongo:27017/docker";
 
   mongoose.set("strictQuery", true);
 
   try {
-    const uri = uris[retryCount];
     const db = await mongoose.connect(uri, options);
     Logger.info(`Connected to Database: ${db.connections[0].name}`);
   } catch (error) {
     Logger.error(`Not Connected to Database ERROR! ${error}`);
-    if (retryCount < uris.length - 1) {
-      // Retry with the next URI
-      await connectToDatabase(retryCount + 1);
-    } else {
-      Logger.error("Failed to connect to any of the databases.");
-    }
   }
 };
 
