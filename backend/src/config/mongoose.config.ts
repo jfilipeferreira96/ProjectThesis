fd77fd5b303598a3f3b627a122c1bdd5b87ccdf2
Filipe Ferreira <filipeferreira96@hotmail.com>
2024-04-06 13:48:06 +0100

    docker changes to support mongo

--- backend/src/config/mongoose.config.ts
+++ backend/src/config/mongoose.config.ts
@@ -2,17 +2,24 @@ import mongoose, { ConnectOptions } from "mongoose";
 import config from "./db.config";
 import Logger from "../utils/logger";
 
-const connectToDatabase = async () => {
+const connectToDatabase = async (retryCount = 0) => {
   const options: ConnectOptions = {};
-  const uri = config.local.localUrlDatabase;
-  
+  const uris = [config.local.localUrlDatabase ?? "mongodb://localhost:27017/docker", "mongodb://mongo:27017/docker", "mongodb://127.0.0.1:27017/docker"];
+
   mongoose.set("strictQuery", true);
 
   try {
+    const uri = uris[retryCount];
     const db = await mongoose.connect(uri, options);
     Logger.info(`Connected to Database: ${db.connections[0].name}`);
   } catch (error) {
     Logger.error(`Not Connected to Database ERROR! ${error}`);
+    if (retryCount < uris.length - 1) {
+      // Retry with the next URI
+      await connectToDatabase(retryCount + 1);
+    } else {
+      Logger.error("Failed to connect to any of the databases.");
+    }
   }
 };
 
