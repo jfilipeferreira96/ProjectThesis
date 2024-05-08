a6a356ead616e12295a31f661c4a3c3348d53a93
Filipe Ferreira <filipeferreira96@hotmail.com>
2024-05-08 20:41:30 +0100

    new docker compose

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -6,11 +6,9 @@ const paramsSerializer = (params: any) => {
   return qs.stringify(params, { arrayFormat: "brackets" });
 };
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
-const isServer = typeof window === "undefined";
 
 const api = axios.create({
-  baseURL: baseURL || "http://51.89.138.159:5000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://51.89.138.159:5000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
@@ -19,24 +17,19 @@ const api = axios.create({
 } as AxiosRequestConfig);
 
 // Add a request interceptor
-api.interceptors.request.use(async (config) => {
-  if (isServer) {
-    const { cookies } = await import("next/headers");
-    const token = cookies().get("token")?.value;
-    if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
-    }
-  } else {
-    const accessToken = localStorage?.getItem("accessToken");
-    const refreshToken = localStorage?.getItem("refreshToken");
+api.interceptors.request.use((config) =>
+{
+  const accessToken = localStorage?.getItem("accessToken");
+  const refreshToken = localStorage?.getItem("refreshToken");
+  
+  if (accessToken)
+  {
+    config.headers["Authorization"] = `Bearer ${accessToken}`;
+  }
 
-    if (accessToken) {
-      config.headers["Authorization"] = `Bearer ${accessToken}`;
-    }
-
-    if (refreshToken) {
-      config.headers["Refresh-Token"] = "refreshToken";
-    }
+  if (refreshToken)
+  {
+    config.headers["Refresh-Token"] = "refreshToken";
   }
 
   return config;
